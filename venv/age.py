b52f983059213f3e12b8efa155d71e697c43dc92
julia <julia040826@126.com>
2019-07-02 21:36:26 +0800

    1st

---
+++ venv/age.py
@@ -0,0 +1,8 @@
+print("please enter your age:")
+i=int(input())
+m=i*12
+print("you have lived for ",m,"months")
+d=m*30
+print("or we can say that you have lived for",d,"days")
+h=d*24
+print("or we can say that you have lived for",h,"hours")
