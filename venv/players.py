3cb0ff3128713ccbec7c229cae89608b092db5af
julia <julia040826@126.com>
2019-07-09 20:46:20 +0800

    This is Juran Zhang's homework for 2019/7/9

---
+++ venv/players.py
@@ -0,0 +1,23 @@
+def input_player():
+    jersey_player={}
+    jersey_number = int(input("please enter the number of the player"))
+    jersey_rating = int(input("please enter the rating of the player"))
+    lst=[]
+    while 0 < jersey_rating <10 and 0 <= jersey_number <100:
+        jersey_player[jersey_number] = jersey_rating
+        jersey_number =int( input("please enter the number of the player"))
+        jersey_rating = int(input("please enter the rating of the player"))
+    else:
+        print("you have now input a number out of range, the loop now break")
+    print(jersey_player)
+    for i in jersey_player:
+        lst.append(i)
+    for a in range(len(lst)):
+        for j in range(len(lst)-1-a):
+            if lst[j]>lst[j+1]:
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+    for b in lst:
+        print("the player with the number of",b,"'s rating is",jersey_player[b])
+
+
+input_player()
