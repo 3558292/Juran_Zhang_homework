b1cd4290e04f9b90967572c37032f027f1bd3d08
julia <julia040826@126.com>
2019-07-10 21:04:24 +0800

    This is Juran Zhang's homework for 2019/7/10

---
+++ venv/menu.py
@@ -0,0 +1,49 @@
+def output_menu():
+    print("please choose a character, remember to write them uncapitalized")
+    print("A-Add players")
+    print("D-Remove players")
+    print("U-update player rating")
+    print("R-Output players above a rating")
+    print("O-Output roster")
+    print("Q-Quit")
+
+
+def input_player():
+    jersey_player={}
+    jersey_number = int(input("please enter the number of the player"))
+    jersey_rating = int(input("please enter the rating of the player"))
+    lst=[]
+    while 0 < jersey_rating <10 and 0 <= jersey_number <100:
+        jersey_player[jersey_number] = jersey_rating
+        jersey_number =int( input("please enter the number of the player"))
+        jersey_rating = int(input("please enter the rating of the player"))
+    else:
+        print("you have now input a number out of range, the loop now break")
+    print(jersey_player)
+    for i in jersey_player:
+        lst.append(i)
+    for b in lst:
+        print("the player with the number of",b,"'s rating number is",jersey_player[b])
+
+
+def choose():
+    x=str(input())
+    if x=="a":
+        print("now please enter the players' jersey number and their rating number")
+        input_player()
+    elif x=="d":
+        pass
+    elif x == "u":
+        pass
+    elif x=="r":
+        pass
+    elif x=="o":
+        pass
+    elif x=="q":
+        print("you now quit the program")
+    else:
+        print("you have enter a number that is not on the menu, please try again")
+
+
+output_menu()
+choose()
